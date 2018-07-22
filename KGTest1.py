78770a4d62c63b8f48f0f3b67ce63741aff16ee1
glasskei <4thekgs@gmail.com>
2018-07-22 18:37:34 -0400

    Initial commit

---
+++ KGTest1.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+
+print("Hello!")
+currentyear = int(input("What year is it?"))
+print("The year is", currentyear)
+print("It has been", abs(1900 - currentyear), " year(s) since the year 1900")
+print("ha"*10)
